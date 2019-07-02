4ec090deb19a183f99d5913a23400e87341c45c0
Wiizard <wiizarddev@gmail.com>
2019-07-02 13:49:39 +0100

    Initial source upload

---
+++ Reiwious/Main.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace Reiwious
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblTitle2 = new System.Windows.Forms.Label();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.lblVer = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Arial", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.lblTitle.Location = new System.Drawing.Point(139, 9);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(193, 46);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Reiwious";
+            // 
+            // lblTitle2
+            // 
+            this.lblTitle2.AutoSize = true;
+            this.lblTitle2.Font = new System.Drawing.Font("Arial", 6.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle2.ForeColor = System.Drawing.Color.Gray;
+            this.lblTitle2.Location = new System.Drawing.Point(149, 50);
+            this.lblTitle2.Name = "lblTitle2";
+            this.lblTitle2.Size = new System.Drawing.Size(94, 12);
+            this.lblTitle2.TabIndex = 1;
+            this.lblTitle2.Text = "by Wiizard and Revox";
+            // 
+            // btnGo
+            // 
+            this.btnGo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(65)))), ((int)(((byte)(65)))));
+            this.btnGo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGo.ForeColor = System.Drawing.Color.Lime;
+            this.btnGo.Location = new System.Drawing.Point(117, 199);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(230, 65);
+            this.btnGo.TabIndex = 2;
+            this.btnGo.Text = "Install";
+            this.btnGo.UseVisualStyleBackColor = false;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(65)))), ((int)(((byte)(65)))));
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.ForeColor = System.Drawing.Color.Lime;
+            this.btnExit.Location = new System.Drawing.Point(420, 3);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(34, 36);
+            this.btnExit.TabIndex = 4;
+            this.btnExit.Text = "X";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInfo.ForeColor = System.Drawing.Color.Lime;
+            this.lblInfo.Location = new System.Drawing.Point(35, 75);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(383, 75);
+            this.lblInfo.TabIndex = 5;
+            this.lblInfo.Text = "An unofficial patch for the IW4x client that attempts to reduce crashes \r\nfrom oc" +
+    "curing.\r\n\r\n\r\nPlease specify your IW4x installation directory:";
+            this.lblInfo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtPath
+            // 
+            this.txtPath.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.txtPath.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtPath.ForeColor = System.Drawing.Color.Lime;
+            this.txtPath.Location = new System.Drawing.Point(47, 154);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.ReadOnly = true;
+            this.txtPath.Size = new System.Drawing.Size(280, 20);
+            this.txtPath.TabIndex = 6;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(65)))), ((int)(((byte)(65)))));
+            this.btnBrowse.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBrowse.Font = new System.Drawing.Font("Arial", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBrowse.ForeColor = System.Drawing.Color.Lime;
+            this.btnBrowse.Location = new System.Drawing.Point(333, 154);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(76, 20);
+            this.btnBrowse.TabIndex = 7;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = false;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // lblVer
+            // 
+            this.lblVer.AutoSize = true;
+            this.lblVer.Font = new System.Drawing.Font("Arial", 5.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblVer.ForeColor = System.Drawing.Color.Gray;
+            this.lblVer.Location = new System.Drawing.Point(319, 39);
+            this.lblVer.Name = "lblVer";
+            this.lblVer.Size = new System.Drawing.Size(8, 7);
+            this.lblVer.TabIndex = 8;
+            this.lblVer.Text = "v";
+            this.lblVer.Click += new System.EventHandler(this.lblVer_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.ClientSize = new System.Drawing.Size(457, 282);
+            this.Controls.Add(this.lblVer);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.lblTitle2);
+            this.Controls.Add(this.lblTitle);
+            this.ForeColor = System.Drawing.Color.Green;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Reiwious";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblTitle2;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblInfo;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Label lblVer;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
---
+++ Reiwious/Main.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Reiwious
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblVer.Text = "v" + Application.ProductVersion;
+            btnGo.Enabled = false;
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.Description = "Please select your IW4x installation folder...";
+            folderBrowserDialog1.ShowDialog();
+            txtPath.Text = folderBrowserDialog1.SelectedPath;
+            btnGo.Enabled = true;
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            if(File.Exists(txtPath.Text + "\\iw4x.dll") && File.Exists(txtPath.Text + "\\iw4x.exe") && Directory.Exists(txtPath.Text + "\\iw4x") && Directory.Exists(txtPath.Text + "\\main") && Directory.Exists(txtPath.Text + "\\miles") && Directory.Exists(txtPath.Text + "\\players") && Directory.Exists(txtPath.Text + "\\zone"))
+            {
+                DialogResult install = MessageBox.Show("A valid IW4x installation was found.\nAre you sure you want to install Reiwious?", "Notice", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+                if (install == DialogResult.Yes)
+                {
+                    btnGo.Enabled = false;
+                    btnExit.Enabled = false;
+                    btnGo.Text = "Installing Reiwious...\nPlease wait...";
+                    File.WriteAllBytes(txtPath.Text + "\\Reiwious.exe", Properties.Resources.Reiwious);
+                    File.WriteAllBytes(txtPath.Text + "\\Reiwious.dll", Properties.Resources.Reiwious1);
+                    if (File.Exists(txtPath.Text + "\\Reiwious.exe") && File.Exists(txtPath.Text + "\\Reiwious.dll"))
+                    {
+                        btnGo.Text = "Installed :)";
+                        btnBrowse.Enabled = false;
+                        btnExit.Enabled = true;
+                        DialogResult installsuccess = MessageBox.Show("Reiwious was installed successfully! :)\n\nDo you want to run it now?", "Install Successful", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+                        if(installsuccess == DialogResult.Yes)
+                        {
+                            System.Diagnostics.Process.Start(txtPath.Text + "\\Reiwious.exe");
+                            Application.Exit();
+                        }
+                        else if(installsuccess == DialogResult.No)
+                        {
+                            MessageBox.Show("Operation was aborted.", "Notice", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("An error occured. Please try again later.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                    }
+                }
+                else if(install == DialogResult.No)
+                {
+                    MessageBox.Show("Operation was aborted.", "Notice", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+            }
+            else
+            {
+                MessageBox.Show("The selected path is not a valid IW4x installation.\nPlease verify the path is correct and try again.", "Notice", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void lblVer_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("All information regarding version history and more can be found at: github.com/Wiizard/Reiwious.\n\nWould you like to visit the github page now?", "Notice", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if(result == DialogResult.Yes)
+            {
+                System.Diagnostics.Process.Start("https://github.com/Wiizard/Reiwious");
+            }
+        }
+    }
+}
